1961501efeba88b726bf9e6a9e3ef47362948657
Psammy <ampadupsamuel1@gmail.com>
2025-05-10 08:01:40 +0000

    initial commit

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -7,12 +7,16 @@ import { gridItems } from '@/data'
 const Grid = () => {
   return (
     <section id='about'>
-      <BentoGrid> {gridItems.map((item) => <BentoGridItem
-        key={item.id}
-        title={item.title} 
-        id={item.id}
-        description={item.description}
-      className={item.className}
+      <BentoGrid> {gridItems.map(({id,title,description,className,imgClassName,img,spareImg,titleClassName}) => <BentoGridItem
+        key={id}
+        title={title} 
+        id={id}
+        description={description}
+        className={className}
+        img={img}
+        imgClassName={imgClassName}
+        spareImg={spareImg}
+        titleClassName={titleClassName}
       />)}</BentoGrid>
    </section>
   )
--- data/index.ts
+++ data/index.ts
@@ -1,3 +1,4 @@
+
 export const navItems = [
   { name: "About", link: "#about" },
   { name: "Projects", link: "#projects" },
