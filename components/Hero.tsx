de677bdbb050c30a805facc653fe0ba3f0c042f8
Psammy <ampadupsamuel1@gmail.com>
2025-07-10 15:02:45 +0000

    changes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -28,12 +28,11 @@ const Hero = () => {
       </div>
         <div className="flex justify-center relative my-20 z-10">
         <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'> 
-          {/* <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>Dynamic web Magic with Next.js</h2> */}
           <TextGenerateEffect
             words='Creating Magic, Building Amazing User Interfaces'
             className='text-center text-[40px] md:text-5xl lg:text-6xl]'
           />
-          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl text-black">
             Hi! I&apos;m Psammy, a Front-end Developer based in Ghana.
           </p>
           
